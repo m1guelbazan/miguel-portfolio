a216ca9adf847147fb2f8eaebfda7eb3499e94d3
m1guelbazan <mbazan4@horizon.csueastbay.edu>
2023-08-03 15:47:24 -0700

    adding wrapper to react app

---
+++ backend_sanity/dist/static/index-8c1ace63-55cd7dd5.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-0e4a6ec2-deccebb6.js";import{D as m}from"./sanity-e83e68e1.js";import"./json-inspector-0b4679d9.js";export{m as DocumentPaneProvider,t as default};
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,16 +1,21 @@
-import React, {useState, useEffect} from 'react'
-import { motion } from 'framer-motion'
-import { images } from '../../constants'
+import React, {useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import { urlFor, client} from '../../client';
 
-import './About.scss'
+import './About.scss';
 
-const abouts = [
-{ title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-{ title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-{ title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03},
-{ title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04}
-];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => { setAbouts(data);
+      })
+  }, [])
+  
   return (
     <>
       <h2 className="head-text"> 
@@ -30,7 +35,7 @@ const About = () => {
           className="app__profile-item"
           key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
