7733100d9addf5b1d0e63f7bf2aaf5961ca8875c
m1guelbazan <mbazan4@horizon.csueastbay.edu>
2023-08-03 13:59:02 -0700

    fixed about section pictures

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -24,15 +24,15 @@ const About = () => {
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app_profile-item"
-            key={about.title + index}
+          whileInView={{ opacity: 1 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className="app__profile-item"
+          key={about.title + index}
           >
             <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop:20}}>{about.title} </h2>
-            <p className="p-text" style={{ marginTop: 10}}>{about.description} </p>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
