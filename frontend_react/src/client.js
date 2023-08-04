924cfbcd47eee208d5c4f42465a89db344b2ac6f
m1guelbazan <mbazan4@horizon.csueastbay.edu>
2023-08-04 01:53:15 -0700

    fixed bug with my sanity client, didnt want to post

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,13 +1,12 @@
-//connect sanity
-import SanityClient from '@sanity/client';
+import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = SanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion: '2022-02-01',
-    useCdn: true,
-    token: process.env.REACT_APP_SANITY_PROJECT_TOKEN,
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-02-01',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN
 });
 
 const builder = imageUrlBuilder(client);
--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,85 +1,86 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState } from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
-import './Footer.scss'
+import './Footer.scss';
 
 const Footer = () => {
-  const [formData,setFormData] = useState({name: '', email: '', message: ''});
-  const [isFormSubmitted,setIsFormSubmitted] = useState(false);
-  const [loading,setLoading] = useState(false);
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const { name, email, message } = formData;
+  const { username, email, message } = formData;
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value});
-  }
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = () => {
     setLoading(true);
+
     const contact = {
       _type: 'contact',
-      name: formData.name,
-      email: email,
-      message: message
-    }
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
 
-    //upload data to sanity
     client.create(contact)
       .then(() => {
         setLoading(false);
         setIsFormSubmitted(true);
       })
-  }
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-      <h2 className="head-text">Take a coffee and chat with me!</h2>
-      
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+
       <div className="app__footer-cards">
-        <div className="app__footer-card">
+        <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@miguel.com" className="p-text">Hello@miguel.com</a>
+          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
         </div>
-
         <div className="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel: +1 (000) 000-0000" className="p-text">+1 (000) 000-0000</a>
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
         </div>
       </div>
-
-    {!isFormSubmitted ? 
-      <div className="app__footer-form app__flex">
-        <div classNamw="app__flex">
-          <input className="p-text" type="text" placeholder="Your Name" name="name" value={name} onChange={handleChangeInput}/>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
         </div>
-
-        <div classNamw="app__flex">
-          <input className="p-text" type="email" placeholder="Your Email " name="email" value={email} onChange={handleChangeInput}/>
-        </div>
-
+      ) : (
         <div>
-          <textarea 
-            className="p-text"
-            placeholder="Your Message"
-            value={message}
-            name={message}
-            onChange={handleChangeInput}
-          />
+          <h3 className="head-text">
+            Thank you for getting in touch!
+          </h3>
         </div>
-        <button type="button" className="p-text" onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'} </button>
-      </div>
-      : <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
-        </div>
-    }
+      )}
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
-  'app__whitebg'
+  'app__whitebg',
 );
