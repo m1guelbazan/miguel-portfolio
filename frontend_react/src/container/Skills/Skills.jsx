1b8b87b5a3171075d2965f3fc0703d347a11595c
m1guelbazan <mbazan4@horizon.csueastbay.edu>
2023-08-03 22:09:41 -0700

    finishing work, starting skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Work.scss';
 
 import './Skills.scss'
 
