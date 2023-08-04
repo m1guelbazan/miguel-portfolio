38a0780de3714b6443556a6194ef900836f0829c
m1guelbazan <mbazan4@horizon.csueastbay.edu>
2023-08-04 00:46:39 -0700

    working on footer

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
 import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className="head-text">Take a coffee and chat with me!</h2>
+      
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+);
